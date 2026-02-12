69e26adb8026d12c0602feafeb88c6ccee94ceea
Amit Kumar Mishra <amit.webperfection@gmail.com>
2026-02-12 16:17:25 +0530

    vite file backend url update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://digitalmarket-h8tb.onrender.com',
         changeOrigin: true
       }
     }

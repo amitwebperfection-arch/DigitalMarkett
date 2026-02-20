e4c3b38425ffbde8fc4112c69e47c40afe15fba7
Amit Kumar Mishra <testweb7876@gmail.com>
2026-02-20 18:30:29 +0530

    contact  api

--- frontend/src/services/Contact.service.js
+++ frontend/src/services/Contact.service.js
@@ -2,10 +2,10 @@ import api from './api';
 
 export const submitContactForm = async (formData) => {
   const { data } = await api.post('/contact', formData);
-  return data; 
+  return data;
 };
 
 export const getAllMessagesAdmin = async () => {
   const { data } = await api.get('/contact/admin/contact');
-  return data; 
+  return data;
 };
